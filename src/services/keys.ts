a9775888113c5f9e25f688a33a93b18e8491e273
Arthur Prydatko <artyrpridatko@gmail.com>
2025-11-26 16:54:38 +0000

    Added users key

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -3,4 +3,5 @@ export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionsKey = (sessionId: string) => `sessions#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames:unique';
-export const userKeysLike = (userId: string) => `users:likes#${userId}`;
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
+export const usernamesKey = () => 'usernames';
