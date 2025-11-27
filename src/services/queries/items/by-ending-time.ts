7c3aa73aa4014a3166884c6c5388790ab4c0eab6
Arthur Prydatko <artyrpridatko@gmail.com>
2025-11-27 10:30:00 +0000

    add sort method

--- src/services/queries/items/by-ending-time.ts
+++ src/services/queries/items/by-ending-time.ts
@@ -15,5 +15,7 @@ export const itemsByEndingTime = async (
 		}
 	});
 
-	console.log(ids);
+	const results = await Promise.all(ids.map(id => client.hGetAll(itemsKey(id))));
+
+	return results.map((item, i) => deserialize(ids[i], item));
 };
